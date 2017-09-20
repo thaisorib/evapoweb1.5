d9dd901ca2600bd06fbecb6163a1d2f8fb3ed5c1
Augusto Soares <gutosoares@outlook.com>
2017-09-19 23:46:03 -0300

    Config routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/pages/Home'
+import PenMon from '@/pages/PenMon'
+import HarSam from '@/pages/HarSam'
+import JenHai from '@/pages/JenHai'
+import Tanque from '@/pages/Tanque'
+import Makkink from '@/pages/Makkink'
+import Linacre from '@/pages/Linacre'
+import Error from '@/pages/Error'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/penmon',
+      name: 'PenMon',
+      component: PenMon
+    },
+    {
+      path: '/linacre',
+      name: 'Linacre',
+      component: Linacre
+    },
+    {
+      path: '/harsam',
+      name: 'HarSam',
+      component: HarSam
+    },
+    {
+      path: '/jenhai',
+      name: 'JanHai',
+      component: JenHai
+    },
+    {
+      path: '/makkink',
+      name: 'Makkink',
+      component: Makkink
+    },
+    {
+      path: '/tanque',
+      name: 'Tanque',
+      component: Tanque
+    },
+    {
+      path: '*',
+      name: 'error',
+      component: Error
     }
   ]
 })
