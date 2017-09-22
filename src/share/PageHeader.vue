6f3a553b399a0b149ef26fd678b18a6f7c692f01
Augusto Soares <gutosoares@outlook.com>
2017-09-22 00:04:38 -0300

    Config header and footer page.

---
+++ src/share/PageHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <nav>
+      <div class="nav-wrapper container">
+        <a href="#" class="brand-logo">EvapoWeb</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/penmon">Penman-Monteith(FAO-56)</router-link></li>
+          <li><router-link to="/linacre">Linacre</router-link><li>
+          <li><router-link to="/harsam">Hargreaves-Samani</router-link></li>
+          <li><router-link to="/jenhai">Jensen-Haise</router-link></li>
+          <li><router-link to="/makkink">Makkink</router-link></li>
+          <li><router-link to="/tanque">Tanque Classe A</router-link></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+
+</style>
