6fa42c6d5fc6366d523a8cc1e91dde731bd3344b
Augusto Soares <gutosoares@outlook.com>
2017-09-28 23:41:08 -0300

    Config global scss on App component.

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,41 @@ export default {
 </script>
 
 <style lang="scss">
+
+html {
+  height: 100%
+}
+
+body {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
 #app {
   background-color: #fafafa;
+  flex: 1;
+
+  nav {
+    background-color: #114E74;
+  }
+
+  .method-description {
+    color: #727273;
+  }
+
+  .card {
+    color: #333;
+
+    .card-content {
+      margin-top: 30px;
+    }
+  }
+
+  .result {
+    padding: 10px 0 0 0;
+    text-align: center;
+    font-size: 2em;
+  }
 }
 </style>
