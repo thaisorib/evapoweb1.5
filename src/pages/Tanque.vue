3e9982c85cc011630a0c41ad94e28299a58caf4a
Augusto Soares <gutosoares@outlook.com>
2017-09-29 21:39:26 -0300

    Config Vuelidate.

--- src/pages/Tanque.vue
+++ src/pages/Tanque.vue
@@ -10,32 +10,35 @@
             </p>
             <br>
             <div class="row">
-              <div class="input-field col m8 s12">
+              <div class="input-field col m6 s12">
                 <input id="bordadura" type="text" class="validate" v-model="bordadura" @input="$v.bordadura.$touch()">
                 <label for="bordadura">Bordadura: (metros)</label>
                 <i class="mi mi-face"></i>
-                <p class="error-message" v-if="!$v.bordadura.between">O valor de bordadura deve ser entre 1m até 1000m</p>
+                <span class="error-message" v-if="!$v.bordadura.between">O valor de bordadura deve ser entre 1m até 1000m</span>
+                <span class="error-message" v-if="!$v.bordadura.required">Este campo é obrigatório</span>
               </div>
             </div>
             <div class="row">
-              <div class="input-field col m8 s12">
+              <div class="input-field col m6 s12">
                 <input id="umidade-relativa" type="text" class="validate" v-model="umiRelativa" @input="$v.umiRelativa.$touch()">
                 <label for="umidade-relativa">Umidade Relativa média diária: (%)</label>
-                <p class="input-message" v-if="!$v.umiRelativa.between">Valor da umidade relativa deve ser entre 30% e 84%</p>
+                <span class="error-message" v-if="!$v.umiRelativa.between">Valor da umidade relativa deve ser entre 30% e 84%</span>
+                <span class="error-message" v-if="!$v.umiRelativa.required">Este campo é obrigatório</span>
               </div>
             </div>
             <div class="row">
-              <div class="input-field col m8 s12">
+              <div class="input-field col m6 s12">
                 <input id="velocidade-vento" type="text" class="validate" v-model="velocidadeVento" @input="$v.velocidadeVento.$touch()">
                 <label for="velocidade-vento">Velocidade do vento a 2 metros: (m/2)</label>
-                <p class="input-message" v-if="!$v.velocidadeVento.between">Valor da velocidade do vento deve ser entre 1m/s e 8m/s</p>
+                <span class="error-message" v-if="!$v.velocidadeVento.between">Valor da velocidade do vento deve ser entre 1m/s e 8m/s</span>
+                <span class="error-message" v-if="!$v.velocidadeVento.required">Este campo é obrigatório</span>
               </div>
             </div>
             <div class="row">
-              <div class="input-field col m8 s12">
+              <div class="input-field col m6 s12">
                 <input id="eca" type="text" class="validate" v-model="eca" @input="$v.eca.$touch()">
                 <label for="eca">ECA: (mm/dia)</label>
-                <p class="input-message" v-if="!$v.eca.required">Este campo é obrigatório</p>
+                <p class="error-message" v-if="!$v.eca.required">Este campo é obrigatório</p>
               </div>
             </div>
             <div class="row" v-if="result">
@@ -56,6 +59,7 @@
 <script>
 import { kp } from '@/functions/kp.js'
 import { eto } from '@/functions/eto.js'
+import { required, between } from 'vuelidate/lib/validators'
 
 export default {
   name: 'tanque',
@@ -65,17 +69,17 @@ export default {
       result: 0,
       methodDescription: 'Esse método consiste em medir diariamente a evapotranspiração de um tanque metálico para estimar a ETo.',
       methodTitle: 'Método Tanque Classe A',
-      bordadura: 0,
-      umiRelativa: 0,
-      velocidadeVento: 0,
-      eca: 0
+      bordadura: '',
+      umiRelativa: '',
+      velocidadeVento: '',
+      eca: ''
     }
   },
 
   validations: {
     bordadura: {
       required,
-      between: between(20, 30)
+      between: between(1, 1000)
     },
     umiRelativa: {
       required,
