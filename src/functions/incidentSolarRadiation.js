c1f473d0a9de6df721eef467e7fb18f2632a9878
Augusto Soares <gutosoares@outlook.com>
2017-10-04 22:25:31 -0300

    Create incident solar radiation function.

---
+++ src/functions/incidentSolarRadiation.js
@@ -0,0 +1,9 @@
+'use strict'
+
+export function incidentSolarRadiation(radiano, insolation, durationDay) {
+  let incidentSolarRadiation
+
+  incidentSolarRadiation = ((a + 0.52 * (brilhoSolar / n)) * ra)
+
+  return incidentSolarRadiation
+}
