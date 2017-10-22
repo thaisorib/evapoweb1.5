511fe645dfee64d82a3a9d084ac17420d30817a7
Augusto Soares <gutosoares@outlook.com>
2017-10-21 22:44:21 -0200

    Config variable on Hargreaves method.

--- src/pages/HarSam.vue
+++ src/pages/HarSam.vue
@@ -11,21 +11,21 @@
             <br>
             <div class="row">
               <div class="input-field col m4">
-                <input id="day" type="text" class="validate" v-model="day" @input="$v.day.$touch()">
+              <input id="day" type="text" class="validate" v-model.number="day" @input="$v.day.$touch()">
                 <label for="day">Dia:</label>
                 <i class="mi mi-face"></i>
                 <span class="error-message" v-if="!$v.day.required">Entre com o valor dia desejado</span>
                 <span class="error-message" v-if="!$v.day.numeric">O valor informado deve ser um número</span>
               </div>
               <div class="input-field col m4">
-                <input id="month" type="text" class="validate" v-model="month" @input="$v.month.$touch()">
+                <input id="month" type="text" class="validate" v-model.number="month" @input="$v.month.$touch()">
                 <label for="month">Mês:</label>
                 <i class="mi mi-face"></i>
                 <span class="error-message" v-if="!$v.month.required">Entre com o valor mês desejado</span>
                 <span class="error-message" v-if="!$v.day.numeric">O valor informado deve ser um número</span>
               </div>
               <div class="input-field col m4">
-                <input id="year" type="text" class="validate" v-model="year" @input="$v.year.$touch()">
+                <input id="year" type="text" class="validate" v-model.number="year" @input="$v.year.$touch()">
                 <label for="year">Ano:</label>
                 <i class="mi mi-face"></i>
                 <span class="error-message" v-if="!$v.year.required">Entre com o valor do ano desejado</span>
@@ -34,40 +34,40 @@
             </div>
             <div class="row">
               <div class="input-field col m8 s12">
-                <input id="temperatureAM" type="text" class="validate" v-model="temperatureAM" @input="$v.temperatureAM.$touch()">
+                <input id="temperatureAM" type="text" class="validate" v-model.number="temperatureAM" @input="$v.temperatureAM.$touch()">
                 <label for="temperatureAM">Temperatura: (9 horas)</label>
                 <span class="error-message" v-if="!$v.temperatureAM.required">Entre com o valor da temperatura às 9 horas</span>
               </div>
             </div>
             <div class="row">
               <div class="input-field col m8 s12">
-                <input id="temperaturePM" type="text" class="validate" v-model="temperaturePM" @input="$v.temperaturePM.$touch()">
+                <input id="temperaturePM" type="text" class="validate" v-model.number="temperaturePM" @input="$v.temperaturePM.$touch()">
                 <label for="temperaturePM">Temperatura: (21 horas)</label>
                 <span class="error-message" v-if="!$v.temperaturePM.required">Entre com o valor da temperatura às 21 horas</span>
               </div>
             </div>
             <div class="row">
               <div class="input-field col m8 s12">
-                <input id="tempMax" type="text" class="validate" v-model="tempMax" @input="$v.tempMax.$touch()">
+                <input id="tempMax" type="text" class="validate" v-model.number="tempMax" @input="$v.tempMax.$touch()">
                 <label for="tempMax">Temperatura Máxima: (˚C)</label>
                 <span class="error-message" v-if="!$v.tempMax.required">Entre com o valor da temperatura máxima</span>
               </div>
             </div>
             <div class="row">
               <div class="input-field col m8 s12">
-                <input id="tempMin" type="text" class="validate" v-model="tempMin" @input="$v.tempMin.$touch()">
+                <input id="tempMin" type="text" class="validate" v-model.number="tempMin" @input="$v.tempMin.$touch()">
                 <label for="tempMin">Temperatura mínima: (˚C)</label>
                 <span class="error-message" v-if="!$v.tempMin.required">Entre com o valor da temperatura mínima</span>
               </div>
             </div>
             <div class="row">
               <div class="input-field col m4 s6">
-                <input id="latitude-graus" type="text" class="validate" v-model="latitudeGraus" @input="$v.latitudeGraus.$touch()">
+                <input id="latitude-graus" type="text" class="validate" v-model.number="latitudeGraus" @input="$v.latitudeGraus.$touch()">
                 <label for="latitude-graus">Latitude em graus:</label>
                 <span class="error-message" v-if="!$v.latitudeGraus.required">Entre com o valor da latitude em graus</span>
               </div>
               <div class="input-field col m4 s6">
-                <input id="latitude-minutos" type="text" class="validate" v-model="latitudeMinutos" @input="$v.latitudeMinutos.$touch()">
+                <input id="latitude-minutos" type="text" class="validate" v-model.number="latitudeMinutos" @input="$v.latitudeMinutos.$touch()">
                 <label for="latitude-minutos">Latitude em minutos:</label>
                 <span class="error-message" v-if="!$v.latitudeMinutos.required">Entre com o valor da latitude em minutos</span>
               </div>
@@ -90,13 +90,13 @@
 <script>
 import { julianDay } from '@/functions/julianDay.js'
 import { latitude } from '@/functions/latitudeHarSam.js'
-import { averageTemperature } from '@/functions/averageTemperature.js'
+import { averageTemp } from '@/functions/averageTemp.js'
 import { solarDeclination } from '@/functions/solarDeclination.js'
 import { sunriseAngle } from '@/functions/sunriseAngle.js'
 import { relativeEarthSun } from '@/functions/relativeEarthSun.js'
 import { radiationAtmosphere } from '@/functions/radiationAtmosphere.js'
 import { etoHarSam } from '@/functions/etoHarSam.js'
-import { required, numeric } from 'vuelidate/lib/validators'
+import { required, numeric, between } from 'vuelidate/lib/validators'
 
 export default {
   name: 'harsam',
@@ -121,11 +121,13 @@ export default {
   validations: {
     day: {
       numeric,
-      required
+      required,
+      between: between(1, 31)
     },
     month: {
       numeric,
-      required
+      required,
+      between: between(1, 12)
     },
     year: {
       numeric,
@@ -153,17 +155,21 @@ export default {
 
   methods: {
     calculate() {
-      let julianDayResult = julianDay(Number(this.day), Number(this.month), Number(this.year))
-      let averageTemperatureResult = averageTemperature(Number(this.temperatureAM), Number(this.temperaturePM), Number(this.tempMax), Number(this.tempMin))
-      let latitudeResult = latitude(Number(this.latitudeGraus), Number(this.latitudeMinutos))
+      let julianDayResult = julianDay(this.day, this.month, this.year)
+      let averageTempResult = averageTemp(this.temperatureAM, this.temperaturePM, this.tempMax, this.tempMin)
+      let latitudeResult = latitude(this.latitudeGraus, this.latitudeMinutos)
       let solarDeclinationResult = solarDeclination(julianDayResult)
       let sunriseAngleResult = sunriseAngle(latitudeResult, solarDeclinationResult)
       let relativeEarthSunResult = relativeEarthSun(julianDayResult)
       let radiationAtmosphereResult = radiationAtmosphere(relativeEarthSunResult, latitudeResult, solarDeclinationResult, sunriseAngleResult)
-      let etoResult = etoHarSam(Number(this.tempMax), Number(this.tempMin), averageTemperatureResult, radiationAtmosphereResult)
+      let etoResult = etoHarSam(this.tempMax, this.tempMin, averageTempResult, radiationAtmosphereResult)
 
       this.result = etoResult.toFixed(2)
     }
+  },
+
+  mounted() {
+    $('select').material_select()
   }
 }
 </script>
