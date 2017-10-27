e80c580890de6376d5becca08396e7cc0dfcab06
Augusto Soares <gutosoares@outlook.com>
2017-10-27 18:40:56 -0200

    Edit not found page and remove hello demo page.

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="error">
-    <h1>Error 404</h1>
+    <h1>404</h1>
     <h3>Página não encontrada!</h3>
+    <router-link class="btn waves-effect waves-light" to="/">Home</router-link>
   </div>
 </template>
 
@@ -11,6 +12,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#error {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 70vw;
 
+  h1 {
+    font-size: 6em;
+  }
+
+  a {
+    margin-top: 2em;
+    background-color: #114E74
+  }
+}
 </style>
