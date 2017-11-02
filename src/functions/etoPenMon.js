e3ef35aedf2d65c555a8cd0548a02aec3ea16bd9
Augusto Soares <gutosoares@outlook.com>
2017-11-02 16:40:36 -0200

    Calculate ETo for PM method.

---
+++ src/functions/etoPenMon.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export function etoPenMon(saturationPressureCurve, psychrometricCoefficient, radiationBalance, modifiedPsychrometricCoefficient, averageTemp, windSpeed, saturationPressure, steamPressure) {
+  return (((saturationPressureCurve / (saturationPressureCurve + modifiedPsychrometricCoefficient)) * ((radiationBalance) * (1 / 2.45))) + ((psychrometricCoefficient / (saturationPressureCurve + modifiedPsychrometricCoefficient)) * (900 / (averageTemp + 275)) * (windSpeed) * (saturationPressure - steamPressure)))
+}
