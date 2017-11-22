2565b354d871b439a9b63414d7eedb0a4ca410c4
Augusto Soares <gutosoares@outlook.com>
2017-11-22 21:22:32 -0200

    Edit not found page.

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,32 +1,48 @@
 <template>
   <div id="error">
-    <h1>404</h1>
-    <h3>Página não encontrada!</h3>
-    <router-link class="btn waves-effect waves-light" to="/">Home</router-link>
+    <PageHeader />
+    <div class="error-message">
+      <h1>Erro 404</h1>
+      <h3>Página não encontrada!</h3>
+      <router-link class="btn waves-effect waves-light" to="/">Home</router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import PageHeader from "../share/PageHeader";
+
 export default {
-  name: 'error'
-}
+  name: "error",
+  components: {
+    PageHeader
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 #error {
+  height: 40vw;
+}
+
+.error-message {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 70vw;
 
   h1 {
     font-size: 6em;
+    color: #114e74;
+  }
+
+  h3 {
+    color: #114e74;
   }
 
   a {
     margin-top: 2em;
-    background-color: #114E74
+    background-color: #114e74;
   }
 }
 </style>
