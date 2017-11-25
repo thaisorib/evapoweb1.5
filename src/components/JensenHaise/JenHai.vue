0e9cea7ee67e009b337b4f8f39c985576bbe44eb
Augusto Soares <gutosoares@outlook.com>
2017-11-25 09:55:20 -0200

    Refactoring JenHai component.

---
+++ src/components/JensenHaise/JenHai.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="jenhai">
+    <PageHeaderEto />
+    <JenHaiForm />
+  </div>
+</template>
+
+<script>
+import PageHeaderEto from '@/share/PageHeaderEto'
+import JenHaiForm from './JenHaiForm'
+
+export default {
+  name: 'jenhai',
+
+  components: {
+    PageHeaderEto,
+    JenHaiForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
