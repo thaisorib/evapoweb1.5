8a92c5507476629820670d146def095aaad4afc0
Augusto Soares <gutosoares@outlook.com>
2017-11-25 09:35:50 -0200

    Refactoring HarSam component.

---
+++ src/components/HargreavesSamani/HarSam.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="harsam">
+    <PageHeaderEto/>
+    <HarSamForm />
+  </div>
+</template>
+
+<script>
+import PageHeaderEto from '@/share/PageHeaderEto'
+import HarSamForm from './HarSamForm'
+
+export default {
+  name: 'harsam',
+
+  components: {
+    PageHeaderEto,
+    HarSamForm
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
