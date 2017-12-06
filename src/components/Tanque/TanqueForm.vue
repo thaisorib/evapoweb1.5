54fb96d341d8e0dd1e8116c8ae78698267d2e387
Augusto Soares <gutosoares@outlook.com>
2017-12-06 19:46:24 -0200

    Convert v-model in number.

--- src/components/Tanque/TanqueForm.vue
+++ src/components/Tanque/TanqueForm.vue
@@ -10,7 +10,7 @@
           <br>
           <div class="row">
             <div class="input-field col m12 s12">
-              <input id="fetch" type="text" class="validate" v-model="fetch" @input="$v.fetch.$touch()">
+              <input id="fetch" type="text" class="validate" v-model.number="fetch" @input="$v.fetch.$touch()">
               <label for="fetch">Bordadura: (metros)</label>
               <span class="error-message" v-if="!$v.fetch.between">O valor de bordadura deve ser entre 1m até 1000m</span>
               <span class="error-message" v-if="!$v.fetch.required">Este campo é obrigatório</span>
@@ -18,7 +18,7 @@
           </div>
           <div class="row">
             <div class="input-field col m12 s12">
-              <input id="daily-humidity" type="text" class="validate" v-model="dailyHumidity" @input="$v.dailyHumidity.$touch()">
+              <input id="daily-humidity" type="text" class="validate" v-model.number="dailyHumidity" @input="$v.dailyHumidity.$touch()">
               <label for="daily-humidity">Umidade Relativa média diária: (%)</label>
               <span class="error-message" v-if="!$v.dailyHumidity.between">Valor da umidade relativa deve ser entre 30% e 84%</span>
               <span class="error-message" v-if="!$v.dailyHumidity.required">Este campo é obrigatório</span>
@@ -26,7 +26,7 @@
           </div>
           <div class="row">
             <div class="input-field col m12 s12">
-              <input id="wind-speed" type="text" class="validate" v-model="windSpeed" @input="$v.windSpeed.$touch()">
+              <input id="wind-speed" type="text" class="validate" v-model.number="windSpeed" @input="$v.windSpeed.$touch()">
               <label for="wind-speed">Velocidade do vento a 2 metros: (m/2)</label>
               <span class="error-message" v-if="!$v.windSpeed.between">Valor da velocidade do vento deve ser entre 1m/s e 8m/s</span>
               <span class="error-message" v-if="!$v.windSpeed.required">Este campo é obrigatório</span>
@@ -34,7 +34,7 @@
           </div>
           <div class="row">
             <div class="input-field col m12 s12">
-              <input id="eca" type="text" class="validate" v-model="eca" @input="$v.eca.$touch()">
+              <input id="eca" type="text" class="validate" v-model.number="eca" @input="$v.eca.$touch()">
               <label for="eca">ECA: (mm/dia)</label>
               <p class="error-message" v-if="!$v.eca.required">Este campo é obrigatório</p>
             </div>
@@ -97,7 +97,7 @@ export default {
       this.result = etoResult.toFixed(2)
 
       this.etoArray.push(this.result)
-      this.$store.dispatch('setEto', this.result)
+      this.$store.dispatch('setEto', Number(this.result))
     }
   }
 }
