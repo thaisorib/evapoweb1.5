4f601910d5bb6aa8abf45c4b62b70c6ade06c1d3
Augusto Soares <gutosoares@outlook.com>
2017-12-09 12:06:18 -0200

    Fix PM method.

--- src/functions/etoPenMon.js
+++ src/functions/etoPenMon.js
@@ -1,5 +1,7 @@
 'use strict'
 
 export function etoPenMon(saturationPressureCurve, psychrometricCoefficient, radiationBalance, modifiedPsychrometricCoefficient, averageTemp, windSpeed, saturationPressure, steamPressure) {
-  return (((saturationPressureCurve / (saturationPressureCurve + modifiedPsychrometricCoefficient)) * ((radiationBalance) * (1 / 2.45))) + ((psychrometricCoefficient / (saturationPressureCurve + modifiedPsychrometricCoefficient)) * (900 / (averageTemp + 275)) * (windSpeed) * (saturationPressure - steamPressure)))
+
+  return (((saturationPressureCurve / (saturationPressureCurve + modifiedPsychrometricCoefficient)) * ((radiationBalance) * (1 / 2.45))) + ((psychrometricCoefficient / (saturationPressureCurve + modifiedPsychrometricCoefficient)) * (900 / (averageTemp + 273)) * (windSpeed) * (saturationPressure - steamPressure)))
+
 }
--- src/functions/relativeEarthSun.js
+++ src/functions/relativeEarthSun.js
@@ -1,9 +1,5 @@
 'use strict'
 
 export function relativeEarthSun(julianDay) {
-  let relativeEarthSun
-
-  relativeEarthSun = (1 + 0.033 * (Math.cos(((2 * Math.PI) / 365) * julianDay)))
-
-  return relativeEarthSun
+  return (1 + 0.033 * (Math.cos(((2 * Math.PI) / 365) * julianDay)))
 }
