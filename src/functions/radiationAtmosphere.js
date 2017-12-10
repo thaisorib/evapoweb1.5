ec802e4fdbebd1e81d496ffd1fc6ebc8759708eb
Thaís Ribeiro <33321445+thaisorib@users.noreply.github.com>
2017-12-10 20:04:16 -0200

    Correct equation Radiation Atmosphere
    This line should not exist: "radiationAtmosphere = (radiationAtmosphere / 2.45)"

--- src/functions/radiationAtmosphere.js
+++ src/functions/radiationAtmosphere.js
@@ -4,7 +4,6 @@ export function radiationAtmosphere(relativeEarthSun, latitude, decSolar, sunris
   let radiationAtmosphere
 
   radiationAtmosphere = (37.586 * relativeEarthSun * (sunriseAngle * Math.sin(latitude) * Math.sin(decSolar) + Math.cos(latitude) * Math.cos(decSolar) * Math.sin(sunriseAngle)))
-  radiationAtmosphere = (radiationAtmosphere / 2.45)
-
+  
   return radiationAtmosphere
 }
