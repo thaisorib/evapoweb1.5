5aeeba733d422c00d8d3dd5605332f4e741eac6f
Thaís Ribeiro <33321445+thaisorib@users.noreply.github.com>
2017-12-12 10:35:42 -0200

    Update kp

--- src/functions/kp.js
+++ src/functions/kp.js
@@ -2,6 +2,6 @@
 
 export function kp(fetch, dailyHumidity, windSpeed) {
 
-  return (0.108 - (0.0286 * windSpeed) + (0.0422 * Math.log(fetch) +
+  return (0.108 - (0.0285984 * windSpeed) + (0.0422 * Math.log(fetch) +
     (0.1434 * Math.log(dailyHumidity)) - (0.000631 * (Math.pow(Math.log(fetch)), 2) * Math.log(dailyHumidity))))
 }
