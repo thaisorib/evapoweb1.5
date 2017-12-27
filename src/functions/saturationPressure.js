6c22bd4d1168ed2a3c1e11e7975abc40790e709e
Augusto Soares <gutosoares@outlook.com>
2017-12-27 20:34:26 -0200

    Fix saturation pressure.

--- src/functions/saturationPressure.js
+++ src/functions/saturationPressure.js
@@ -1,5 +1,5 @@
 'use strict'
 
 export function saturationPressure(averageTemp) {
-  return (0.6108 * (Math.pow(((17.27 * averageTemp) / (237.3 + averageTemp)), 10)));
+  return (0.6108 * (Math.pow(10, ((7.5 * averageTemp) / (237.3 + averageTemp)))));
 }
