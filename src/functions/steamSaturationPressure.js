5b67381d6fcbd08c2ff4a3809371f01d1d86e896
Thaís Ribeiro <33321445+thaisorib@users.noreply.github.com>
2017-12-27 09:28:34 -0200

    Update steamSaturationPressure.js

--- src/functions/steamSaturationPressure.js
+++ src/functions/steamSaturationPressure.js
@@ -3,7 +3,7 @@
 export function saturationPressure(tempMedia) {
   let saturationPressure
 
-  saturationPressure = (0.6108 * (Math.pow(10, ((17.27 * tempMedia) / (237.3 + tempMedia)))))
+  saturationPressure = (0.6108 * (Math.pow(((17.27 * tempMedia) / (237.3 + tempMedia)), 10)))
 
   return saturationPressure
 }
