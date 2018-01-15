8cefe5dbc09d6119867f71cd94a7743de2dc2181
Augusto Soares <gutosoares@outlook.com>
2018-01-15 21:15:16 -0200

    Formatted final result.

--- src/components/Lamina/Results.vue
+++ src/components/Lamina/Results.vue
@@ -4,8 +4,7 @@
       <div class="card white darken-1">
         <div class="card-content">
           <span class="card-title">Resultado Final:</span>
-          {{laminaBruta}}
-            <h5 v-if="laminaBruta > 0">Olá! Você precisa irrigar {{ laminaBruta }} milímetros hoje!</h5>
+            <h5 v-if="laminaBruta > 0">Olá! Você precisa irrigar {{ laminaBruta.toFixed(2) }} milímetros hoje!</h5>
             <h5 v-else>Olá! Você não necessita irrigar, pois durante esses dias a chuva foi suficiente para atender a necessidade hídrica da sua cultura!</h5>
         </div>
       </div>
